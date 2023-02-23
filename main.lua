eb8ff280aa9161aa5631df311d6c40be177db061
Aldie NightStar <aldie.nightstar@gmail.com>
2023-02-23 23:25:15 +0100

    kb tweak

--- main.lua
+++ main.lua
@@ -64,5 +64,14 @@ function love.gamepadpressed(joystick, button)
     -- Curently no sense which joystick
     -- We use buttons from all of them (FIXME)
     -- button could be: dpleft, dpright, dpup, dpdown, a, b
-    control:emit(joymap[button])
+    local k = joymap[button]
+    if k ~= nil then control:emit(k) end
 end
+
+-- keyboard
+function love.keypressed(key, scancode, isrepeat)
+    -- We map 'space' and 'return' key as 'a' and 'b'
+    if key == 'space'      then key = 'a'
+    elseif key == 'return' then key = 'b' end
+    control:emit(key)
+ end
