dc3167cd71c716fcc55bcae07a863974a0d0fc52
Aldie NightStar <aldie.nightstar@gmail.com>
2023-02-24 10:01:49 +0100

    better tiles

--- gfx.lua
+++ gfx.lua
@@ -1,10 +1,16 @@
-gfx = {tileSize=16}
+gfx = {tileSize=18}
+
+local function modColor(color)
+    return {color[1]*1.5, color[2]*1.5, color[3]*1.5}
+end
 
 function gfx.drawTile(x, y, color)
     if color == nil then color = {1, 1, 1} end
     local sz = gfx.tileSize
-    love.graphics.setColor(unpack(color))
+    love.graphics.setColor(unpack(modColor(color)))
     love.graphics.rectangle('fill', x*sz, y*sz, sz, sz)
+    love.graphics.setColor(unpack(color))
+    love.graphics.rectangle('fill', x*sz+1, y*sz+1, sz-1, sz-1)
 end
 
 function gfx.centerTileLine(x, y, x2, y2, color)
